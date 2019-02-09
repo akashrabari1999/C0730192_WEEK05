3886178a6fdb8a393b49bf1917635687b30eda6c
visualstudio <visualstudio@122-PC40>
2019-02-08 19:35:04 -0500

    hkj

--- C0730195_WEEK5/Program.cs
+++ C0730195_WEEK5/Program.cs
@@ -10,6 +10,41 @@ namespace C0730195_WEEK5
     {
         static void Main(string[] args)
         {
+            Elevator e = new Elevator();
+            e.run();
+        }
+    }
+
+    class Node
+    {
+        public Node() { }
+        public Node floor;
+        public Node elevatorUp;
+        public string FloorNumber;
+    }
+
+    class Elevator
+    {
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void run()
+        {
+            FirstFloor = new Node();
+            SecondFloor = new Node();
+            ThirdFloor = new Node();
+            FourthFloor = new Node();
+
+            FirstFloor.FloorNumber = "First Floor";
+            FirstFloor.elevatorUp = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+            SecondFloor.elevatorUp = ThirdFloor;
+            ThirdFloor.FloorNumber = "Third Floor";
+            ThirdFloor.elevatorUp = FourthFloor;
+            FourthFloor.FloorNumber = "Fourth Floor";
+            FourthFloor.elevatorUp = null;
         }
     }
 }
